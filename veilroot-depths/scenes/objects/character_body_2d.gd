edabb777054275ce5334f84f3fca506090cbd274
MalteFP <MalteFp@outlook.com>
2026-09-07 08:45:34 +0200

    Fixed lights

--- veilroot-depths/scenes/objects/character_body_2d.gd
+++ veilroot-depths/scenes/objects/character_body_2d.gd
@@ -5,9 +5,8 @@ const SPEED = 50.0
 const JUMP_VELOCITY = -150.0
 var currentCollisionMask = 1
 
-func _ready():
-	get_tree().current_scene.get_node("Tilemap/Layer" + str((currentCollisionMask % get_tree().current_scene.get_meta("numLayers")) + 1)).set_self_modulate(Color(0.173, 0.173, 0.173, 1.0))
-	
+func _ready() -> void:
+		get_tree().current_scene.get_node("Tilemap/Layer" + str(currentCollisionMask)).set_light_mask(1)
 
 func _physics_process(delta: float) -> void:
 	applyMovement(delta)
@@ -33,13 +32,13 @@ func applyMovement(delta: float) -> void:
 
 func swapLayer():
 	set_collision_mask_value(currentCollisionMask, false)
-	get_tree().current_scene.get_node("Tilemap/Layer" + str(currentCollisionMask)).z_index = 0
-	get_tree().current_scene.get_node("Tilemap/Layer" + str(currentCollisionMask)).set_self_modulate(Color(0.173, 0.173, 0.173, 1.0))
+	get_tree().current_scene.get_node("Tilemap/Layer" + str(currentCollisionMask)).z_index = 1
+	get_tree().current_scene.get_node("Tilemap/Layer" + str(currentCollisionMask)).set_light_mask(0)
 	currentCollisionMask = (currentCollisionMask % get_tree().current_scene.get_meta("numLayers")) + 1
 	set_collision_mask_value(currentCollisionMask, true)
 	get_node("AnimatedSprite2D/PointLight2D").set_item_shadow_cull_mask(int("0b" + str(1 << (currentCollisionMask -1)) + "1" ))
 	get_tree().current_scene.get_node("Tilemap/Layer" + str(currentCollisionMask)).z_index = 1
-	get_tree().current_scene.get_node("Tilemap/Layer" + str(currentCollisionMask)).set_self_modulate(Color(1.0, 1.0, 1.0, 1.0))
+	get_tree().current_scene.get_node("Tilemap/Layer" + str(currentCollisionMask)).set_light_mask(1)
 	
 	
 	
