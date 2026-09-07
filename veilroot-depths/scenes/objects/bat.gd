7dcf2f03b2ba0ab80572dbd49dd7b02e0a2a3316
MalteFP <MalteFp@outlook.com>
2026-09-07 10:25:03 +0200

    Fixed bat

--- veilroot-depths/scenes/objects/bat.gd
+++ veilroot-depths/scenes/objects/bat.gd
@@ -4,15 +4,17 @@ extends Node2D
 
 @onready var finalPos = get_node("FinalPosMarker").global_position
 
+var appeared = false
+
 func _ready() -> void:
 	sprite.visible = false
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 func _on_appear_box_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
-	if body.name == "player"
+	if body.name == "player" and appeared == false:
+		appeared = true
 		sprite.scale = Vector2(0,0)
 		sprite.visible = true
 		sprite.play("idle")
@@ -20,7 +22,8 @@ func _on_appear_box_body_shape_entered(body_rid: RID, body: Node2D, body_shape_i
 			sprite.scale += Vector2(0.05,0.05)
 			await get_tree().create_timer(0.1).timeout
 		sprite.play("flying")
-		while not global_position == finalPos:
-			global_position = global_position.move_toward(finalPos, 1)
+		while not sprite.global_position == finalPos:
+			sprite.global_position = sprite.global_position.move_toward(finalPos, 1)
 			await get_tree().create_timer(0.01).timeout
 		sprite.play("idle")
+		get_node("AnimatedSprite2D/Sprite2D").visible = true
